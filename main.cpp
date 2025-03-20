eafd8f0fb47e0c9f7b30e7dc67f51463c1745db9
descarteslover <joeyjudedeniro@gmail.com>
2025-03-19 19:44:47 -0500

    Completed MakeMove function for Knight forge ahead Added an global array for the possible moves a Knight Will make Added my test files to the folder to test in the coming days Added numRows and numCols as global variables to make for ease of calling functions which make use of it

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "Queue.h"
+#include "Stack.h"
 
 using namespace std;
+const int TOTAL_MOVE_CHOICES = 8;
+const char possibleMoves[8] = { 'A','B','C','D','E','F','G','H' };
+int NUM_ROWS;
+int NUM_COLS;
 
 bool CheckMove(int** board, int row, int col, const int numRows, const int numCols, char move) {
 	switch (move) {
@@ -63,8 +69,72 @@ bool CheckMove(int** board, int row, int col, const int numRows, const int numCo
 	}
 }
 
-void MakeMove(int i, int j, char move); //forge ahead
-void UndoMove(int i, int j, char move); //backtracking
+void MakeMove(int** board, int rowPos, int colPos, char move, int& moveNumber) { //forge ahead
+	switch (move) {
+	case 'A':
+		rowPos -= 1;
+		colPos += 2;
+		board[rowPos][colPos] = moveNumber;
+		break;
+	case 'B':
+		rowPos -= 2;
+		colPos -= 1;
+		board[rowPos][colPos] = moveNumber;
+		break;
+	case 'C':
+		rowPos += 1;
+		colPos -= 2;
+		board[rowPos][colPos] = moveNumber;
+		break;
+	case 'D':
+		rowPos += 2;
+		colPos += 1;
+		board[rowPos][colPos] = moveNumber;
+		break;
+	case 'E':
+		rowPos -= 2;
+		colPos += 1;
+		board[rowPos][colPos] = moveNumber;
+		break;
+	case 'F':
+		rowPos -= 1;
+		colPos -= 2;
+		board[rowPos][colPos] = moveNumber;
+		break;
+	case 'G':
+		rowPos += 2;
+		colPos -= 1;
+		board[rowPos][colPos] = moveNumber;
+		break;
+	case 'H':
+		rowPos += 1;
+		colPos += 2;
+		board[rowPos][colPos] = moveNumber;
+		break;
+	}
+	moveNumber++;
+}
+
+Queue<char>* FindPossibleMoves(int** board, const int rowPos, const int colPos, const int numRows, const int numCols) {
+	Queue<char>* queue = new Queue<char>;
+	for (int i = 0; i < TOTAL_MOVE_CHOICES; i++) {
+		if (CheckMove(board, rowPos, colPos, numRows, numCols, possibleMoves[i])){
+			queue->Enqueue(possibleMoves[i]);
+		}
+	}
+	return queue;
+}
+
+void UndoMove(int** board, int i, int j, char move, int& moveNumber) {//backtracking //FIX ME COME BACK TO THIS
+
+}
+
+bool IsTourClosed(int** board, int startRow, int startCol, const int numRows, const int numCols) { //PlaceQueens analog
+	int moveNum = 0;
+	Stack<Queue<char>*> stack;
+	Queue<char>* queue = FindPossibleMoves(board, )
+
+} 
 int main() {
 	const int numCols = 3;
 	const int numRows = 3;
